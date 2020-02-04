ac1293f2c2aafb45464d5e81e13d2ecae3d69e14
Mohy9 <jakub.zlamal9@gmail.com>
2020-02-04 13:57:59 +0100

    Updates

---
+++ precache-manifest.39f3add86c61a3ad4ae5962c5d50f3b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbe5ef9007ac77e514070b51f14bc9f",
+    "url": "/ultimate-weather-app/index.html"
+  },
+  {
+    "revision": "e84f271385d1f2fd635a",
+    "url": "/ultimate-weather-app/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "46f69ebf7f913b6de1a0",
+    "url": "/ultimate-weather-app/static/css/main.2e5f0e08.chunk.css"
+  },
+  {
+    "revision": "e84f271385d1f2fd635a",
+    "url": "/ultimate-weather-app/static/js/2.4d3b4903.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ultimate-weather-app/static/js/2.4d3b4903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f69ebf7f913b6de1a0",
+    "url": "/ultimate-weather-app/static/js/main.578747b5.chunk.js"
+  },
+  {
+    "revision": "10aa7bcc4fdff5cf0a63",
+    "url": "/ultimate-weather-app/static/js/runtime-main.7959c134.js"
+  },
+  {
+    "revision": "18ddf2b375f7cb200b55544ee907381f",
+    "url": "/ultimate-weather-app/static/media/Prequel.18ddf2b3.ttf"
+  }
+]);
